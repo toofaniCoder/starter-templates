1a8405138f71cb261d8f6aa65f033cd23f908693
toofaniCoder <smartcoder500@gmail.com>
2023-09-05 00:05:16 +0530

    Next JS + MUI Starter Code

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <main>
+      <Typography variant="h3">Hello! Next JS</Typography>
+    </main>
+  );
+}
