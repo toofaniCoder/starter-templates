b2e7161a409f98f48bfd1fabe6f12fb781d2ad39
toofaniCoder <smartcoder500@gmail.com>
2023-09-15 15:05:07 +0530

    starter code v1

---
+++ README.md
@@ -0,0 +1,5 @@
+## Project Structure
+![Project Structure](https://res.cloudinary.com/dxrbrkfvv/image/upload/v1694770440/github/nextjs_joy_ui_structure_fk8xuy.png)
+
+## Template Home Page View
+![Home Page View](https://res.cloudinary.com/dxrbrkfvv/image/upload/v1694770385/github/nextjs_joy_ui_hello_world_mua7x2.png)
---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/joy";
+
+export default function Home() {
+  return (
+    <main>
+      <Typography
+        textAlign={"center"}
+        fontWeight={300}
+        level="h1"
+      >
+        Hello! Next JS + Material Joy UI
+      </Typography>
+      {/* <h2>heading 2</h2> */}
+    </main>
+  );
+}
